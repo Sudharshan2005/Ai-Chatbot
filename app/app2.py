3013b80dff85d106d77809f5f4b9ee420b127d79
sudharshan <leelasudharshan2005@gmail.com>
2025-10-31 14:26:20 +0530

    Deployment without model

--- app/app2.py
+++ app/app2.py
@@ -722,4 +722,4 @@ def on_agent_leave(data):
         emit("agent_left", {"agent_id": agent_id})
 
 if __name__ == "__main__":
-    socketio.run(app, port=5001, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5001, debug=False)
